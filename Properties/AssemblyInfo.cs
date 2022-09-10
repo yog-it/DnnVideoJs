81fe90b342c94eec306c514251754c69c919b2ac
Ryan Jagdfeld <ryan@jagdfeld.com>
2022-09-09 19:15:25 -0500

    other changes to versioning

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("01.00.03")]
-[assembly: AssemblyFileVersion("01.00.03")]
+[assembly: AssemblyVersion("0.2.1")]
+[assembly: AssemblyFileVersion("0.2.1")]
